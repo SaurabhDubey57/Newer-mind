d23bbf19fb033feeb25e21a5a7819a32a016304c
Saurabh Dubey <59115657+Death-cure@users.noreply.github.com>
2020-10-27 17:24:05 +0530

    cheak

---
+++ q.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+  using namespace std;
+int main(){
+int a;
+cout<< "enter the first no "<<endl;
+cin>>a;
+int b;
+cout<< "enter the second no "<<endl;
+cin >>b;
+
+cout<< "sum of those no is "<<a+b<<endl;
+return 0;
+}
---
+++ use of void 10.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int x;
+
+cout<< "enter your favrate no"<<endl;
+cin>> x ;
+cout << "favrate no is"<< x
+<<endl;
+return 0;
+}
