7fe6e84116dba997b23cedb8e93c5ee47b5f89a5
Saurabh Dubey <59115657+Death-cure@users.noreply.github.com>
2020-10-27 17:27:13 +0530

    Update q.cpp

--- q.cpp
+++ q.cpp
@@ -2,12 +2,12 @@
   using namespace std;
 int main(){
 int a;
-cout<< "enter the first no "<<endl;
+cout<< "enter the first no"<<endl;                  // for first number which you enter.
 cin>>a;
 int b;
 cout<< "enter the second no "<<endl;
-cin >>b;
+cin>>b;
 
-cout<< "sum of those no is "<<a+b<<endl;
+cout<<"sum of those no is "<<a+b<<endl;
 return 0;
 }
