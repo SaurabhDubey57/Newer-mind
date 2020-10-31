59f2636f8dc95a005670012f0be655d54ba1cd33
Sundaram Dubey <56407566+maze-runnar@users.noreply.github.com>
2020-10-31 17:39:18 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main () {
+
+ freopen ("input.txt","r",stdin);
+ freopen ("output.txt", "w", stdout);
+
+  int t;
+  cin >> t;
+  while(t--){
+
+
+  }
+  return 0;
+}
