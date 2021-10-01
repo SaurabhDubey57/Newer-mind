1db3fdb32293857e39ed788aee29464f8008c687
Sundaram Dubey <56407566+maze-runnar@users.noreply.github.com>
2021-10-01 20:07:15 +0530

    Create vector.cpp

---
+++ vector.cpp
@@ -0,0 +1,9 @@
+#include<bits/c++.h>
+using namespace std;
+int main() {
+  int n = 10;
+  vector<int> v(n, 0); //initializing vector
+  for(auto i: v) { // iterating vector
+    cout << i << " ";
+  }
+}
